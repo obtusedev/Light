318879b4214b20ae945d975f9e7e92dca9baeecf
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-03-19 19:33:02 -0400

    Add validation to name data
    Validate that name is alpha (a-zA-Z) only and ignoring spaces and /.

--- src/controllers/doctor.controller.js
+++ src/controllers/doctor.controller.js
@@ -1,12 +1,20 @@
 const { fetchDoctorDetails } = require("../helpers/data.js");
+const validator = require("validator");
 
 function createDoctor(req, res) {
     const { name } = req.body;
-    const doctor = fetchDoctorDetails(name);
-    res.json({
-        status: 200,
-        doctor
-    });
+    if (validator.isAlpha(name, "en-US", { ignore: "- /" })) {
+        const doctor = fetchDoctorDetails(name);
+        res.status(200).json({
+            status: 200,
+            doctor,
+        });
+    } else {
+        res.status(400).json({
+            status: 400,
+            msg: "Bad Request",
+        });
+    }
 }
 
 module.exports = {
