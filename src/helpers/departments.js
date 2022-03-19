57fe594aab0b2d66b406195247d235f9e019a579
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-03-19 13:19:44 -0400

    Add list of medical fields
    Medical fields for randomly generated data

---
+++ src/helpers/departments.js
@@ -0,0 +1,23 @@
+// list of medical departments
+
+const departments = [
+    "Anesthesiology",
+    "Dentistry",
+    "Dermatology",
+    "Emergency Medicine",
+    "Family Medicine",
+    "Gynecology",
+    "Internal Medicine",
+    "Neurology",
+    "Pediatric",
+    "Physical Medicine",
+    "Psychiatry",
+    "Psycology",
+    "Radiology",
+    "Urology",
+    "Veterinary",
+];
+
+module.exports = {
+    departments: departments
+};
