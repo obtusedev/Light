f289b8d6d45a760247c00e3f35ec454874cafc54
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-03-19 13:22:28 -0400

    Update routes

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ const app = express();
 const PORT = 3000;
 
 /* Routes */
+const doctorRoutes = require("./routes/doctor.route.js");
 
 /* Middleware */
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/api/v1/doctor", doctorRoutes);
 
 app.get("/", (req, res) => {
     res.send("Welcome!");
