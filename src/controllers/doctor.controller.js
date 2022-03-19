368022a786a00760d3c83e2142a274a37f0cfed1
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-03-19 19:55:44 -0400

    Add doctor instances to db

--- src/controllers/doctor.controller.js
+++ src/controllers/doctor.controller.js
@@ -1,13 +1,23 @@
 const { fetchDoctorDetails } = require("../helpers/data.js");
 const validator = require("validator");
+const Doctor = require("../models/Doctor.js");
 
 function createDoctor(req, res) {
     const { name } = req.body;
     if (validator.isAlpha(name, "en-US", { ignore: "- /" })) {
         const doctor = fetchDoctorDetails(name);
-        res.status(200).json({
-            status: 200,
-            doctor,
+        Doctor.create(doctor, err => {
+            if (err) {
+                res.status(400).json({
+                    status: 400,
+                    msg: err.message,
+                });
+            } else {
+                res.status(200).json({
+                    status: 200,
+                    doctor,
+                });
+            }
         });
     } else {
         res.status(400).json({
