22b0fe2b5cfe9be8c8f9dda7987348dfe8bdb592
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-03-19 23:34:18 -0400

    Add endpoint to get list of doctors from a clinic

--- src/controllers/clinic.controller.js
+++ src/controllers/clinic.controller.js
@@ -21,6 +21,30 @@ function getClinics(req, res) {
     });
 }
 
+function getDoctorsFromClinic(req, res) {
+    const { clinic } = req.params;
+    const re = new RegExp(clinic, "i"); // 'i' for case insensitive
+    Doctor.find({ clinic: re }, "-_id -__v").exec((err, doctors) => {
+        if (err) {
+            res.status(400).json({
+                status: 400,
+                msg: err.message,
+            });
+        } else if (doctors.length === 0) {
+            res.status(400).json({
+                status: 400,
+                msg: "No doctors found for this clinic",
+            });
+        } else {
+            res.status(200).json({
+                status: 200,
+                doctors,
+            });
+        }
+    });
+}
+
 module.exports = {
     getClinics: getClinics,
+    getDoctorsFromClinic: getDoctorsFromClinic,
 };
--- src/routes/clinic.route.js
+++ src/routes/clinic.route.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { getClinics } = require("../controllers/clinic.controller.js");
+const { getClinics, getDoctorsFromClinic } = require("../controllers/clinic.controller.js");
 
 router.get("/", getClinics);
+router.get("/:clinic/doctors", getDoctorsFromClinic);
 
 module.exports = router;
