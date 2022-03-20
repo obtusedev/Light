b7c23d231b8ce304a35b212cd02ef1b8b3dbbc0b
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-03-20 16:36:57 -0400

    Add middleware to check if clinic exists
    This helps clarify whether the clinic doesn't exists or that there is
    not doctors for that clinic.

--- src/routes/clinic.route.js
+++ src/routes/clinic.route.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getClinics, getDoctorsFromClinic } = require("../controllers/clinic.controller.js");
+const { getClinics, getClinic, getDoctorsFromClinic } = require("../controllers/clinic.controller.js");
+const {checkIfClinicExists} = require("../helpers/exist.js");
+
+router.use("/:clinic", checkIfClinicExists);
 
 router.get("/", getClinics);
+router.get("/:clinic", getClinic);
 router.get("/:clinic/doctors", getDoctorsFromClinic);
 
 module.exports = router;
