cff90e0219c91cd8b4a88f42d02bf4f7785c9cb4
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-03-21 15:53:41 -0400

    Refactor middleware
    Refactor middleware that checks if clinic exists to use JSONResponse.

--- src/helpers/exist.js
+++ src/helpers/exist.js
@@ -1,19 +1,14 @@
 const Doctor = require("../models/Doctor.js");
+const { JSONResponse } = require("../helpers/response.js");
 
 function checkIfClinicExists(req, res, next) {
     const { clinic } = req.params;
     const re = new RegExp(clinic, "i");
     Doctor.exists({ clinic: re }, (err, result) => {
         if (err) {
-            res.status(400).json({
-                status: 400,
-                msg: err.message,
-            });
+            return new JSONResponse(400, err.message).send(req, res);
         } else if (result === null) {
-            res.status(404).json({
-                status: 404,
-                msg: `Clinic: ${clinic} not found`,
-            });
+            return new JSONResponse(404, `Clinic: ${clinic} not found`).send(req, res);
         } else {
             next();
         }
