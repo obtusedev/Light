915366ce85a5e94fe67f7d0d51a0c9b579c0852b
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-03-21 15:59:15 -0400

    Add endpoint to search by clinic name

--- src/controllers/clinic.controller.js
+++ src/controllers/clinic.controller.js
@@ -1,6 +1,23 @@
 const Doctor = require("../models/Doctor.js");
 const { JSONResponse } = require("../helpers/response.js");
 
+function getClinic(req, res) {
+    const { clinic } = req.params;
+    const re = new RegExp(clinic, "i");
+    Doctor.find({ clinic: re }, "-_id -__v", (err, clinic) => {
+        if (err) {
+            return new JSONResponse(400, err.message).send(req, res);
+        } else if (clinic.length === 0) {
+            return new JSONResponse(404, `No clinic: ${clinic} found`).send(
+                req,
+                res
+            );
+        } else {
+            return new JSONResponse(200, clinic).send(req, res);
+        }
+    });
+}
+
 function getClinics(req, res) {
     Doctor.find({}).distinct("clinic", (err, clinics) => {
         if (err) {
@@ -20,7 +37,10 @@ function getDoctorsFromClinic(req, res) {
         if (err) {
             return new JSONResponse(400, err.message).send(req, res);
         } else if (doctors.length === 0) {
-            return new JSONResponse(404, "No doctors found for this clinic").send(req, res);
+            return new JSONResponse(
+                404,
+                "No doctors found for this clinic"
+            ).send(req, res);
         } else {
             return new JSONResponse(200, doctors).send(req, res);
         }
@@ -28,6 +48,7 @@ function getDoctorsFromClinic(req, res) {
 }
 
 module.exports = {
+    getClinic: getClinic,
     getClinics: getClinics,
     getDoctorsFromClinic: getDoctorsFromClinic,
 };
--- src/routes/clinic.route.js
+++ src/routes/clinic.route.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getClinics, getClinic, getDoctorsFromClinic } = require("../controllers/clinic.controller.js");
-const {checkIfClinicExists} = require("../helpers/exist.js");
+const {
+    getClinics,
+    getClinic,
+    getDoctorsFromClinic,
+} = require("../controllers/clinic.controller.js");
+const { checkIfClinicExists } = require("../helpers/exist.js");
 
 router.use("/:clinic", checkIfClinicExists);
 
